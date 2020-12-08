d1b9cd7f4ca3a498ed0036ece274a08db60e58ad
askomarov <askomarov13@mail.ru>
2020-12-08 21:59:24 +0300

    главная страница почти готова)))

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,7 @@ let mainMenu = document.querySelector('.header-menu__wrapper');
 window.addEventListener("resize", function () {
   if (window.innerWidth > 1439.98) {
     mainMenu.classList.remove("header-menu__wrapper--shown");
-    navToggle.setAttribute("aria-expanded", "false");
+    menuButton.setAttribute("aria-expanded", "false");
   }
 }
 );
@@ -25,13 +25,39 @@ function toggleMenu() {
     }
   });
 };
+
+
+let tarifPopupSection = document.querySelector('.business-tarif');
+let showTarifPopupBtn = document.querySelector('.pricing__link-business-tarif');
+let closetarifPopupBtn = document.querySelector('.business-tarif__close-popup');
+
+function showPopup(btn, section, classToShow) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    section.classList.add(classToShow);
+  })
+};
+function closePopup(btn, section, classToShow) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    section.classList.remove(classToShow);
+  });
+  document.addEventListener('keyup', function () {
+    if (keyCode = 27) {
+      section.classList.remove(classToShow);
+    }
+  });
+};
+
+
 document.addEventListener('DOMContentLoaded', function () {
   toggleMenu();
+  showPopup(showTarifPopupBtn, tarifPopupSection, 'business-tarif--show-popup');
+  closePopup(closetarifPopupBtn, tarifPopupSection, 'business-tarif--show-popup');
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   navToggle.addEventListener("click", function () {
-//     console.log('нажал');
 
-//   });
+// document.addEventListener('keyup', function (event) {
+//   console.log('Key: ', event.key);
+//   console.log('keyCode: ', event.keyCode);
 // });
